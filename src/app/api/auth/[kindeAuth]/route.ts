fc68b6523363cf50e4cd6722a309128772e82141
sohittaksande <sohittaksande2003@gmail.com>
2025-09-02 19:23:17 +0530

    Fix route for Vercel deployment

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,13 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { kindeAuth: string } } // destructured correctly
-) {
-  const { kindeAuth } = params;
-  const response = await handleAuth(request, kindeAuth);
-  
-  // handleAuth usually returns a Response object
-  return response;
+export async function GET(request: NextRequest) {
+  return handleAuth(request);
 }
