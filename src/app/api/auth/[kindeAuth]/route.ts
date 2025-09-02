35cbf546fd2bda750b3cd0c0ebca5fb8ff347ec2
sohittaksande <sohittaksande2003@gmail.com>
2025-09-02 20:26:21 +0530

    Fix route for Vercel deployment

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,5 +1,10 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
 
-// App Router only expects a single argument: the request
-export const GET = handleAuth;
-export const POST = handleAuth; // if POST is needed
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
+}
