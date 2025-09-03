39900761865d7feb64a8d0605c800e22e457c81d
sohittaksande <sohittaksande2003@gmail.com>
2025-09-03 16:41:12 +0530

    fix: update GET handler

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,3 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 
-export async function GET(request: NextRequest) {
-  const url = new URL(request.url)
-  const endpoint = url.pathname.split('/').pop() // grabs [kindeAuth]
-  if (!endpoint) {
-    return new Response('Missing endpoint', { status: 400 })
-  }
-  return handleAuth(request, endpoint)
-}
+export const GET = handleAuth();
