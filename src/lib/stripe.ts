2f0c8ecf3a4d3be216860d9805a9b3e8f723e4e3
sohittaksande <sohittaksande2003@gmail.com>
2025-09-03 16:59:24 +0530

    fix: update GET handler

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,9 @@
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import Stripe from "stripe"
+
 import { PLANS } from '../config/stripe'
 import { db } from '../db'
+import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
   apiVersion: "2023-10-16" as Stripe.LatestApiVersion
