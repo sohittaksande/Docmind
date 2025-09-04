3e403f813618974387d62c04f2e23da66e5eeae2
sohittaksande <sohittaksande2003@gmail.com>
2025-09-05 00:04:35 +0530

    stripe

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       // Prevent webpack from bundling "canvas" on the client
---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+export const config = {
+  matcher: ['/dashboard/:path*', '/auth-callback'],
+}
+
+export default withAuth
